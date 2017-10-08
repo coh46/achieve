3c4bd5ff375c61286d9698995e66e309480a7e51
Ubuntu <coh46r@gmail.com>
2017-10-08 17:50:06 +0000

    フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
      registrations: "users/registrations",
@@ -18,6 +22,10 @@ Rails.application.routes.draw do
 
   resources :poems, only: [:index, :show] do
   end
+
+  resources :users, only: [:index, :show]
+
+  resources :relationships, only: [:create, :destroy]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
